a9d66c47012704f0ced165898336da78414f6831
regirock365 <hemedi@protonmail.com>
2021-08-19 14:56:20 +0930

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,63 +47,64 @@ const Home: React.FC<Props> = () => {
   return (
     <>
       <div
-        className="bg-cover bg-top"
+        className="bg-cover bg-top absolute"
         style={{
           backgroundImage:
             "url(https://res.cloudinary.com/projectgroceries/image/upload/v1626779778/regirock365/wp7420571_fg16eo.jpg)",
         }}
       >
-        <div className="bg-gradient-to-b from-white/25 to-white backdrop-blur pb-8 lg:pb-20">
-          <header className="flex items-center">
-            <img
-              src="https://pbs.twimg.com/profile_images/1291555016155619329/9uGuMPfy_400x400.jpg"
-              alt="Picture of me"
-              className="w-32 h-32 m-2 rounded-full shadow-lg border-2 border-amber-500"
-            />
-            <div className="ml-3">
-              <h1>regirock365</h1>
-              <span className="text-xl">Web Developer</span>
+        <div className="h-96 w-screen bg-gradient-to-b from-white/25 to-white backdrop-blur" />
+      </div>
+      <div className="pb-8 sticky">
+        <header className="flex items-center">
+          <img
+            src="https://pbs.twimg.com/profile_images/1291555016155619329/9uGuMPfy_400x400.jpg"
+            alt="Picture of me"
+            className="w-32 h-32 m-2 rounded-full shadow-lg border-2 border-amber-500"
+          />
+          <div className="ml-3">
+            <h1>regirock365</h1>
+            <span className="text-xl">Web Developer</span>
+          </div>
+        </header>
+        <section id="online" className="flex items-center p-3 pt-0">
+          <div className="w-32 m-2 h-2" />
+          <div>
+            <div className="text-xl flex items-center gap-2">
+              Github:
+              <a
+                href="https://github.com/regirock365"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                regirock365
+              </a>
             </div>
-          </header>
-          <section id="online" className="flex items-center p-3 pt-0">
-            <div className="w-32 m-2 h-2" />
-            <div>
-              <div className="text-xl flex items-center gap-2">
-                Github:
-                <a
-                  href="https://github.com/regirock365"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  regirock365
-                </a>
-              </div>
-              <div className="text-xl flex items-center gap-2">
-                Twitter:
-                <a
-                  href="https://twitter.com/regirock365"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  regirock365
-                </a>
-              </div>
-              <div className="text-xl flex items-center gap-2">
-                Unsplash:
-                <a
-                  href="https://unsplash.com/@regirock365"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  @regirock365
-                </a>
-              </div>
+            <div className="text-xl flex items-center gap-2">
+              Twitter:
+              <a
+                href="https://twitter.com/regirock365"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                regirock365
+              </a>
             </div>
-          </section>
-        </div>
+            <div className="text-xl flex items-center gap-2">
+              Unsplash:
+              <a
+                href="https://unsplash.com/@regirock365"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                @regirock365
+              </a>
+            </div>
+          </div>
+        </section>
       </div>
 
-      <section className="p-3">
+      <section className="p-3 sticky">
         <h2 className="mb-3">Projects</h2>
         {projects.map((project) => (
           <div
